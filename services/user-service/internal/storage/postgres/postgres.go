d8eaae43dda10fcf6ba753e61000854b1f9c6f24
Aleksandr Vinogradov <vin89441@gmail.com>
2025-09-24 17:46:09 +0300

    add insert user, get user

--- services/user-service/internal/storage/postgres/postgres.go
+++ services/user-service/internal/storage/postgres/postgres.go
@@ -2,9 +2,9 @@ package postgres
 
 import (
 	"context"
-	"time"
 
 	"github.com/erknas/ecom/user-service/internal/config"
+	"github.com/erknas/ecom/user-service/internal/domain/models"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,9 +13,6 @@ type PostgresPool struct {
 }
 
 func New(ctx context.Context, cfg *config.Config) (*PostgresPool, error) {
-	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
-	defer cancel()
-
 	config, err := newPoolConfig(cfg)
 	if err != nil {
 		return nil, err
@@ -31,4 +28,41 @@ func New(ctx context.Context, cfg *config.Config) (*PostgresPool, error) {
 	}
 
 	return &PostgresPool{pool: pool}, nil
+}
+
+func (p *PostgresPool) InsertUser(ctx context.Context, user *models.User) (int64, error) {
+	query := "INSERT INTO users(first_name, phone_number, email, password_hash, created_at) VALUES($1, $2, $3, $4, $5) RETURNING id"
+
+	var id int64
+	if err := p.pool.QueryRow(
+		ctx,
+		query,
+		user.FirstName,
+		user.PhoneNumber,
+		user.Email,
+		user.PasswordHash,
+		user.CreatedAt,
+	).Scan(&id); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (p *PostgresPool) User(ctx context.Context, id int64) (*models.User, error) {
+	query := "SELECT id, first_name, phone_number, email, password_hash, created_at FROM users WHERE id = $1"
+
+	row := p.pool.QueryRow(ctx, query, id)
+
+	user := new(models.User)
+	if err := row.Scan(&user.ID,
+		&user.FirstName,
+		&user.PhoneNumber,
+		&user.Email,
+		&user.PasswordHash,
+		&user.CreatedAt); err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
