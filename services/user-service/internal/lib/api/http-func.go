56cc096022d1924e7a6bdbd4e7febea0314842e3
Aleksandr Vinogradov <vin89441@gmail.com>
2025-09-24 17:42:36 +0300

    http helpers

---
+++ services/user-service/internal/lib/api/http-func.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type httpFunc func(w http.ResponseWriter, r *http.Request) error
+
+func MakeHTTPFunc(fn httpFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
+		defer cancel()
+
+		if err := fn(w, r.WithContext(ctx)); err != nil {
+			WriteJSON(w, http.StatusBadRequest, map[string]any{
+				"status_code": http.StatusBadRequest,
+				"error":       err,
+			})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
