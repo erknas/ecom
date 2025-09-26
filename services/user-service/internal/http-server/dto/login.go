eefdeb895bbc183fcc426e40d58d6c88ccda3511
Aleksandr Vinogradov <vin89441@gmail.com>
2025-09-26 16:39:05 +0300

    login dtos

---
+++ services/user-service/internal/http-server/dto/login.go
@@ -0,0 +1,15 @@
+package dto
+
+type LoginRequest struct {
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
