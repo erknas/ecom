6e05151335aff1d095378316dd5cd3e151a66bab
Aleksandr Vinogradov <vin89441@gmail.com>
2025-10-02 17:10:27 +0300

    change user dto; add validation for CreateUserRequest, UpdateUserRequest

--- services/user-service/internal/http/dto/user.go
+++ services/user-service/internal/http/dto/user.go
@@ -1,20 +1,23 @@
 package dto
 
-import "time"
+import (
+	"net/mail"
+	"strings"
+	"time"
+	"unicode/utf8"
+)
 
 type User struct {
-	ID          int64     `json:"id"`
-	FirstName   string    `json:"first_name"`
-	PhoneNumber string    `json:"phone_number"`
-	Email       string    `json:"email"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID        int64     `json:"id"`
+	FirstName string    `json:"first_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type CreateUserRequest struct {
-	FirstName   string `json:"first_name"`
-	PhoneNumber string `json:"phone_number"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
+	FirstName string `json:"first_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
 type CreateUserResponse struct {
@@ -23,8 +26,116 @@ type CreateUserResponse struct {
 }
 
 type UpdateUserRequest struct {
-	FirstName   *string `json:"first_name"`
-	PhoneNumber *string `json:"phone_number"`
-	Email       *string `json:"email"`
-	Password    *string `json:"password"`
+	FirstName *string `json:"first_name"`
+	Email     *string `json:"email"`
+	Password  *string `json:"password"`
+}
+
+type UpdateUserResponse struct {
+	ID      int64  `json:"id"`
+	Message string `json:"message"`
+}
+
+func (req CreateUserRequest) Validate() map[string]string {
+	errors := make(map[string]string)
+
+	if errMsg := validateFirstName(req.FirstName); errMsg != "" {
+		errors["first_name"] = errMsg
+	}
+
+	if !validateEmail(req.Email) {
+		errors["email"] = "invalid email format"
+	}
+
+	if errMsg := validatePassword(req.Password); errMsg != "" {
+		errors["password"] = errMsg
+	}
+
+	return errors
+}
+
+func (req UpdateUserRequest) Validate() map[string]string {
+	errors := make(map[string]string)
+
+	if req.FirstName != nil {
+		if errMsg := validateFirstName(*req.FirstName); errMsg != "" {
+			errors["first_name"] = errMsg
+		}
+	}
+
+	if req.Email != nil {
+		if !validateEmail(*req.Email) {
+			errors["email"] = "invalid email format"
+		}
+	}
+
+	if req.Password != nil {
+		if errMsg := validatePassword(*req.Password); errMsg != "" {
+			errors["password"] = errMsg
+		}
+	}
+
+	return errors
+}
+
+func validateFirstName(name string) string {
+	nameValue := strings.TrimSpace(name)
+
+	if utf8.RuneCountInString(nameValue) == 0 {
+		return "first name must not be empty"
+	}
+
+	if utf8.RuneCountInString(nameValue) < 2 {
+		return "first name must be at least 2 characters"
+	}
+
+	if utf8.RuneCountInString(nameValue) > 16 {
+		return "first name too long"
+	}
+
+	return ""
+}
+
+func validateEmail(email string) bool {
+	emailValue := strings.TrimSpace(email)
+
+	if len(emailValue) == 0 {
+		return false
+	}
+
+	addr, err := mail.ParseAddress(emailValue)
+	if err != nil {
+		return false
+	}
+
+	parts := strings.Split(addr.Address, "@")
+
+	local := parts[0]
+	domain := parts[1]
+
+	if len(local) == 0 {
+		return false
+	}
+
+	if len(domain) == 0 {
+		return false
+	}
+
+	if !strings.Contains(domain, ".") {
+		return false
+	}
+
+	if strings.HasPrefix(domain, ".") || strings.HasSuffix(domain, ".") {
+		return false
+	}
+
+	return true
+}
+
+func validatePassword(password string) string {
+	if len(password) < 8 {
+		return "password must be at least 8 characters long"
+	}
+
+	return ""
 }
