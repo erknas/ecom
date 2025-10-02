7f80f82d793b55ddeab9d05d84c4faa5d9835e09
Aleksandr Vinogradov <vin89441@gmail.com>
2025-10-02 17:11:40 +0300

    update logs

--- services/user-service/internal/service/service.go
+++ services/user-service/internal/service/service.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/erknas/ecom/user-service/internal/domain/models"
 	"github.com/erknas/ecom/user-service/internal/http/dto"
-	"github.com/erknas/ecom/user-service/internal/lib/jwt"
+	"github.com/erknas/ecom/user-service/internal/storage"
 	"go.uber.org/zap"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -18,40 +18,38 @@ var (
 type UserRepository interface {
 	InsertUser(ctx context.Context, user *models.User) (int64, error)
 	UserByID(ctx context.Context, id int64) (*models.User, error)
-	UserByPhoneNumber(ctx context.Context, phoneNumber string) (*models.User, error)
+	UserByEmail(ctx context.Context, email string) (*models.User, error)
 	Update(ctx context.Context, id int64, user *models.UpdatedUser) error
 }
 
-type JWTManager interface {
-	GenerateAccessToken(userID int64, phoneNumber string) (string, error)
-	GenerateRefreshToken(userID int64) (string, error)
-	ValidateAccessToken(tokenString string) (*jwt.Claims, error)
+type TokenGenerator interface {
+	GenerateAccessToken(userID int64, email string) (string, error)
 }
 
 type Service struct {
-	userRepo   UserRepository
-	jwtManager JWTManager
-	log        *zap.Logger
+	userRepo  UserRepository
+	generator TokenGenerator
+	log       *zap.Logger
 }
 
-func New(userRepo UserRepository, jwtManager JWTManager, log *zap.Logger) *Service {
+func New(userRepo UserRepository, generator TokenGenerator, log *zap.Logger) *Service {
 	return &Service{
-		userRepo:   userRepo,
-		jwtManager: jwtManager,
-		log:        log,
+		userRepo:  userRepo,
+		generator: generator,
+		log:       log,
 	}
 }
 
 func (s *Service) CreateNewUser(ctx context.Context, req *dto.CreateUserRequest) (*dto.CreateUserResponse, error) {
-	user, err := models.NewUser(req.FirstName, req.PhoneNumber, req.Email, req.Password)
+	user, err := models.NewUser(req.FirstName, req.Email, req.Password)
 	if err != nil {
-		s.log.Error("new user error", zap.Error(err))
+		s.log.Error("new user failure", zap.Error(err))
 		return nil, err
 	}
 
 	id, err := s.userRepo.InsertUser(ctx, user)
 	if err != nil {
-		s.log.Error("insert new user error", zap.Error(err))
+		s.log.Error("insert user failure", zap.Error(err))
 		return nil, err
 	}
 
@@ -64,23 +62,45 @@ func (s *Service) CreateNewUser(ctx context.Context, req *dto.CreateUserRequest)
 func (s *Service) GetUser(ctx context.Context, id int64) (*dto.User, error) {
 	user, err := s.userRepo.UserByID(ctx, id)
 	if err != nil {
-		s.log.Error("user by id error", zap.Error(err), zap.Int64("user_id", id))
+		s.log.Error("user by id failure", zap.Error(err))
 		return nil, err
 	}
 
 	return &dto.User{
-		ID:          user.ID,
-		FirstName:   user.FirstName,
-		PhoneNumber: user.PhoneNumber,
-		Email:       user.Email,
-		CreatedAt:   user.CreatedAt,
+		ID:        user.ID,
+		FirstName: user.FirstName,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+	}, nil
+}
+
+func (s *Service) UpdateUser(ctx context.Context, id int64, req *dto.UpdateUserRequest) (*dto.UpdateUserResponse, error) {
+	user, err := models.NewUpdatedUser(req.FirstName, req.Email, req.Password)
+	if err != nil {
+		s.log.Error("new updated user failure", zap.Error(err))
+		return nil, err
+	}
+
+	if err := s.userRepo.Update(ctx, id, user); err != nil {
+		s.log.Error("update user failure", zap.Error(err))
+		return nil, err
+	}
+
+	return &dto.UpdateUserResponse{
+		ID:      id,
+		Message: "user updated",
 	}, nil
 }
 
 func (s *Service) Login(ctx context.Context, req *dto.LoginRequest) (*dto.LoginResponse, error) {
-	user, err := s.userRepo.UserByPhoneNumber(ctx, req.PhoneNumber)
+	user, err := s.userRepo.UserByEmail(ctx, req.Email)
 	if err != nil {
-		s.log.Error("user by phone number error", zap.Error(err))
+		if errors.Is(err, storage.ErrUserNotFound) {
+			s.log.Warn("user by email failure", zap.Error(err))
+			return nil, ErrInvalidCredentials
+		}
+
+		s.log.Error("user by email failure", zap.Error(err))
 		return nil, err
 	}
 
@@ -89,45 +109,14 @@ func (s *Service) Login(ctx context.Context, req *dto.LoginRequest) (*dto.LoginR
 		return nil, ErrInvalidCredentials
 	}
 
-	accessToken, err := s.jwtManager.GenerateAccessToken(user.ID, req.PhoneNumber)
+	token, err := s.generator.GenerateAccessToken(user.ID, req.Email)
 	if err != nil {
-		s.log.Error("generate access token error", zap.Error(err))
-		return nil, err
-	}
-
-	refreshToken, err := s.jwtManager.GenerateRefreshToken(user.ID)
-	if err != nil {
-		s.log.Error("generate refresh token error", zap.Error(err))
+		s.log.Error("generate access token failure", zap.Error(err))
 		return nil, err
 	}
 
 	return &dto.LoginResponse{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
+		ID:          user.ID,
+		AccessToken: token,
 	}, nil
-}
-
-func (s *Service) ValidateAccessToken(ctx context.Context, token string) (*jwt.Claims, error) {
-	claims, err := s.jwtManager.ValidateAccessToken(token)
-	if err != nil {
-		s.log.Error("validate access token error", zap.Error(err))
-		return nil, err
-	}
-
-	return claims, nil
-}
-
-func (s *Service) UpdateUser(ctx context.Context, id int64, req *dto.UpdateUserRequest) error {
-	user, err := models.NewUpdatedUser(req.FirstName, req.PhoneNumber, req.Email, req.Password)
-	if err != nil {
-		s.log.Error("new user error", zap.Error(err))
-		return err
-	}
-
-	if err := s.userRepo.Update(ctx, id, user); err != nil {
-		s.log.Error("update user error", zap.Error(err), zap.Int64("user_id", id))
-		return err
-	}
-
-	return nil
 }
