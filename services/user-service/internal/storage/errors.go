6ccedd14107396a17c382f803b354ab90e7e5f81
Aleksandr Vinogradov <vin89441@gmail.com>
2025-10-10 17:31:22 +0300

    remove ErrNoChanges

--- services/user-service/internal/storage/errors.go
+++ services/user-service/internal/storage/errors.go
@@ -5,6 +5,5 @@ import "errors"
 var (
 	ErrUserExists       = errors.New("user already exists")
 	ErrUserNotFound     = errors.New("user not found")
-	ErrNoChanges        = errors.New("nothing to update")
 	ErrInternalDatabase = errors.New("internal database")
 )
